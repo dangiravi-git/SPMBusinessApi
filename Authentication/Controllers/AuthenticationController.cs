70343e94f2fbbfda65d8d7b93ad638a98e43c379
dangiravi@gmail.com <76875942+dangiravi-git@users.noreply.github.com>
2023-11-10 15:55:07 +0530

    authinticate

---
+++ Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet("GetAuthenticate")]
+        public async Task<ActionResult<bool>> GetAuthenticate(string key)
+        {
+            if (key == "abc")
+            {
+                return Ok(true);
+            }
+            else
+            {
+                return Ok(false);
+            }
+        }
+    }
+}
